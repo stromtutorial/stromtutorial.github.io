a6765981e82a2f92fb9bcaa70f8175c11207a961
Paul Lewis <paul.lewis@uconn.edu>
2019-10-30 22:13:06 -0400

    removing src folders from web site

--- linux/steps/step-12/src/go.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-LD_LIBRARY_PATH="$HOME/lib" ./strom $@
-
--- win/steps/step-01/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>	///start
-
-int main(int argc, const char * argv[]) {
-    std::cout << "Hello World!" << std::endl;
-
-    return 0;
-} ///end
